c3e813e721660007622a2c838e771aaf734806fc
coreocto <coreocto@hotmail.com>
2019-01-03 12:01:06 +0800

    Add control on io buffer size

--- jbelib.py
+++ jbelib.py
@@ -15,33 +15,31 @@ def log(msg):
     if CONST_DEBUG:
         print(msg)
 
+def replace_chars(s):
+    return s.replace(':','_').replace('\\','_').replace('.','_').replace(' ', '_')
 
-def encode(source, target):
+def encode(source, target, buffer_size = 4096):
     temp = 0
     statinfo = os.stat(source)
     file_size = statinfo.st_size
     bit_cnt = 0
     cnt = 0
 
-    tmp_data1 = os.path.basename(source).replace(':', '_').replace(' ', '_').replace('.', '_')+'_data1.bin'
-    tmp_data2 = os.path.basename(source).replace(':', '_').replace(' ', '_').replace('.', '_')+'_data2.bin'
+    tmp_data1 = replace_chars(os.path.basename(source))+'_data1.bin'
+    tmp_data2 = replace_chars(os.path.basename(source))+'_data2.bin'
     tmp_data1 = os.path.join("temp", tmp_data1)
     tmp_data2 = os.path.join("temp", tmp_data2)
 
     try:
-        with open(source, "rb") as file_in:
-            with open(tmp_data1, "wb") as data1_file:
-                with open(tmp_data2, "wb") as data2_file:
+        with open(source, "rb", buffer_size) as file_in:
+            with open(tmp_data1, "wb", buffer_size) as data1_file:
+                with open(tmp_data2, "wb", buffer_size) as data2_file:
                     copy_buf = True
                     while copy_buf:
-                        bytes_to_read = CONST_4K
 
-                        if file_size % CONST_4K == 0:
-                            pass
-                        elif file_size - (cnt * CONST_4K) > CONST_4K:
-                            pass
-                        else:
-                            bytes_to_read = file_size % CONST_4K
+                        bytes_to_read = min(buffer_size, file_size - (cnt * buffer_size))
+                        if bytes_to_read <= 0:
+                            break
 
                         copy_buf = file_in.read(bytes_to_read)
 
@@ -78,17 +76,17 @@ def encode(source, target):
                         data2_file.write(bytes([temp]))
                         temp = 0
 
-        with open(target, "wb") as file_out:
+        with open(target, "wb", buffer_size) as file_out:
             log(str(file_size))
             file_out.write(struct.pack(">Q", file_size))
 
-            with open(tmp_data1, "rb") as data1_file:
+            with open(tmp_data1, "rb", buffer_size) as data1_file:
                 shutil.copyfileobj(data1_file, file_out)
-            # os.remove(tmp_data1)
+            os.remove(tmp_data1)
 
-            with open(tmp_data2, "rb") as data2_file:
+            with open(tmp_data2, "rb", buffer_size) as data2_file:
                 shutil.copyfileobj(data2_file, file_out)
-            # os.remove(tmp_data2)
+            os.remove(tmp_data2)
 
             file_out.flush()
     except PermissionError as identifier:
@@ -97,21 +95,16 @@ def encode(source, target):
         return 0
 
 
-def decode(source, target):
+def decode(source, target, buffer_size = 4096):
     statinfo = os.stat(source)
     file_size = statinfo.st_size
 
-    # tmp_data1 = r'r:\data3.bin'
-    # tmp_data2 = r'r:\data4.bin'
-    # tmp_data1 = os.path.basename(source).replace(':', '_').replace(' ', '_').replace('.', '_')+'_data3.bin'
-    tmp_data2 = os.path.basename(source).replace(':', '_').replace(' ', '_').replace('.', '_')+'_data4.bin'
-    # tmp_data1 = os.path.join("temp", tmp_data1)
+    tmp_data2 = replace_chars(os.path.basename(source))+'_data4.bin'
     tmp_data2 = os.path.join("temp", tmp_data2)
 
-    with open(source, "rb") as file_in:
-        with open(target, "wb") as file_out:
-            # with open(tmp_data1, "wb+") as data1_file:
-            with open(tmp_data2, "wb+") as data2_file:
+    with open(source, "rb", buffer_size) as file_in:
+        with open(target, "wb", buffer_size) as file_out:
+            with open(tmp_data2, "wb+", buffer_size) as data2_file:
                 # first four bytes are the file size
                 file_size_bytes = struct.unpack(">Q", file_in.read(8))
                 file_size_bytes = file_size_bytes[0]
@@ -122,6 +115,7 @@ def decode(source, target):
                     data2_size += 1
 
                 data2_start = file_size - data2_size
+                data1_size = file_size - data2_size - 8
 
                 file_in.seek(data2_start)
 
@@ -130,61 +124,85 @@ def decode(source, target):
                 data2_file.flush()
 
                 data2_file.seek(0)
-                # data1_file.seek(0)
                 file_in.seek(8,0)
-                # file_in.read(8)
 
                 byte_written = 0
 
                 max_bit = 8
                 bit_shift = 7
 
-                while True:
-                    one_byte = data2_file.read(1)
-                    if one_byte:
-                        one_byte = ord(one_byte)
-                    else:
-                        one_byte = 0
+                cnt_data1 = 0
+                pos_data1 = 0
+                buf_data1 = None
 
-                    if (byte_written + 8 > file_size_bytes):
-                        log('file_size_bytes = '+str(file_size_bytes))
-                        max_bit = (file_size_bytes % 8)
-                        log('209, max_bit: '+str(max_bit))
+                cnt = 0
+                copy_buf = True
+                while copy_buf:
 
-                        # becoz original file size is not a multiple of 8 bits
-                        # need to trim unncessary bits
-                        one_byte = one_byte >> (8-max_bit)
-                        
-                        bit_shift = max_bit - 1
-                        log('218, bit_shift: '+str(bit_shift))
-
-                    for j in reversed(range(max_bit)):
-                        try:
-                            if one_byte >> (j) & 1 == 1:
-                                tmp = file_in.read(1)
-                                if tmp:
-                                    file_out.write(tmp)
-                                    byte_written += 1
-                                else:
-                                    log(str(one_byte >> (bit_shift - j)))
-                                    log('file_size_bytes = ' + str(file_size_bytes))
-                                    log('tmp = '+str(tmp))
-                                    log('max_bit = '+str(max_bit))
-                                    log('one_byte = '+str(one_byte))
-                                    log('bit_shift = '+str(bit_shift))
-                                    log('j = '+str(j))
-                                    log('byte_written = ' + str(byte_written))
-                            else:
-                                file_out.write(b"\0")
-                                byte_written += 1
-                        except Exception as identifier:
-                            log('Ex: '+str(bit_shift))
-                            log('Ex: '+str(j))
-                            log('Ex: '+identifier)
-                            raise
-                            # pass
-
-                    if byte_written >= file_size_bytes:
+                    bytes_to_read = min(buffer_size, data2_size - (cnt * buffer_size))
+                    if bytes_to_read <= 0:
                         break
 
-        file_out.flush()
+                    copy_buf = data2_file.read(bytes_to_read)
+
+                    if not copy_buf:
+                        break
+
+                    for one_byte in copy_buf:
+                        if one_byte:
+                            pass
+                        else:
+                            one_byte = 0
+
+                        if (byte_written + 8 > file_size_bytes):
+                            log('file_size_bytes = '+str(file_size_bytes))
+                            max_bit = (file_size_bytes % 8)
+                            log('209, max_bit: '+str(max_bit))
+
+                            # becoz original file size is not a multiple of 8 bits
+                            # need to trim unncessary bits
+                            one_byte = one_byte >> (8-max_bit)
+                            
+                            bit_shift = max_bit - 1
+                            log('218, bit_shift: '+str(bit_shift))
+
+                        for j in reversed(range(max_bit)):
+                            try:
+                                if one_byte >> (j) & 1 == 1:
+
+                                    if (not buf_data1) or (pos_data1 == len(buf_data1)):
+                                        bytes_to_read_data1 = min(buffer_size, data1_size - (cnt_data1 * buffer_size))
+                                        buf_data1 = file_in.read(bytes_to_read_data1)
+                                        cnt_data1 += 1
+                                        pos_data1 = 0
+                                    
+                                    tmp = buf_data1[pos_data1]
+                                    pos_data1 += 1
+
+                                    if tmp:
+                                        file_out.write(bytes([tmp]))
+                                        byte_written += 1
+                                    else:
+                                        log(str(one_byte >> (bit_shift - j)))
+                                        log('file_size_bytes = ' + str(file_size_bytes))
+                                        log('tmp = '+str(tmp))
+                                        log('max_bit = '+str(max_bit))
+                                        log('one_byte = '+str(one_byte))
+                                        log('bit_shift = '+str(bit_shift))
+                                        log('j = '+str(j))
+                                        log('byte_written = ' + str(byte_written))
+                                else:
+                                    file_out.write(b"\0")
+                                    byte_written += 1
+                            except Exception as identifier:
+                                log('Ex: '+str(bit_shift))
+                                log('Ex: '+str(j))
+                                log('Ex: '+str(identifier))
+                                raise
+
+                        if byte_written >= file_size_bytes:
+                            break
+                    
+                    cnt += 1
+
+            file_out.flush()
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import jbelib
 import hashlib
 import os
+import datetime
 
 def replace_chars(s):
     return s.replace(':','_').replace('\\','_').replace('.','_')
@@ -13,18 +14,31 @@ if __name__ == '__main__':
             if os.path.isfile(fullpath):
                 tmp_name = replace_chars(filename) + '.enc'
                 tmp_path = os.path.join('enc_output',tmp_name)
-                result = jbelib.encode(fullpath, tmp_path)
+                start_time = datetime.datetime.now()
+                result = jbelib.encode(fullpath, tmp_path, 88)
+                end_time = datetime.datetime.now()
+                time_difference  = end_time - start_time
+                print('encode time: '+str(divmod(time_difference.days * 86400 + time_difference.seconds, 60)))
                 if result == 0:
+                    # pass
                     tmp_name2 = replace_chars(filename)
                     tmp_path2 = os.path.join('dec_output',tmp_name2)
+                    start_time = datetime.datetime.now()
                     jbelib.decode(tmp_path,tmp_path2)
+                    end_time = datetime.datetime.now()
+                    time_difference  = end_time - start_time
+                    print('decode time: '+str(divmod(time_difference.days * 86400 + time_difference.seconds, 60)))
                     m = hashlib.sha1()
-                    for line in open(fullpath,'rb'):
-                        m.update(line)
+                    with open(fullpath, 'rb') as f:
+                        for chunk in iter(lambda: f.read(8192), b''): 
+                            m.update(chunk)
                     hash1 = m.hexdigest()
                     m = hashlib.sha1()
-                    for line in open(tmp_path2,'rb'):
-                        m.update(line)
+                    with open(tmp_path2, 'rb') as f:
+                        for chunk in iter(lambda: f.read(8192), b''): 
+                            m.update(chunk)
                     hash2 = m.hexdigest()
                     if (hash1 != hash2):
                         print(hash1+','+hash2+','+fullpath)
+                else:
+                    print("process failed for file: "+fullpath)
